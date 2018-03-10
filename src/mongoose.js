1559d2090ef9426f354f9eca40a3c440e0c822d6
Florian Lenz <flenz81@googlemail.com>
2018-03-10 12:12:41 +0100

    [db] fix mongo logging

--- src/mongoose.js
+++ src/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = require('mongoose').Schema;
 const logger = require('./logger');
 
-mongoose.connect(process.env.MONGO_URL, logger.error);
+mongoose.connect(process.env.MONGO_URL, {
+    useMongoClient: true
+}, logger.error);
 
 module.exports = {
     Message: mongoose.model('Message', new Schema({
